ac9e4563733eba6043446c297c2533885a96d4d4
Bertrand Paquet <bertrand.paquet@gmail.com>
2020-09-08 22:10:05 +0200

    Update

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,4 +1,4 @@
 Gem.pre_install do|_|
   puts 'Executing .gem_install_hook'
-  system('./.gem_install_hook') || raise.('execution of .gem_install_hook failed.')
+  system('./.gem_install_hook') || raise('execution of .gem_install_hook failed.')
 end
