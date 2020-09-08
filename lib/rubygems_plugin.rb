6bdcea98903104dc64f009ef6670f7c419da64bb
Bertrand Paquet <bertrand.paquet@gmail.com>
2020-09-08 22:13:55 +0200

    Update

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,6 +1,4 @@
 Gem.pre_install do|_|
-  puts 'Executing .gem_install_hook'
-  puts Dir.pwd
-  p ENV
-  system('./.gem_install_hook') || raise('execution of .gem_install_hook failed.')
+  puts "Executing .gem_install_hook in #{ENV['PWD']}"
+  system("cd #{ENV['PWD']} && ./.gem_install_hook") || raise('execution of .gem_install_hook failed.')
 end
